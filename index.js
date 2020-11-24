5fb396bf501a97875b9416b767a45d1f56c5bd0b
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-11-24 18:43:57 -0500

    set up files

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// start your server here
+const server = require("./server");
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`\n Server is running on localhost:${port}\n`);
+});
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,20 @@
 module.exports = {
   development: {
-    // complete your knexfile
+    client: "sqlite3",
+    useNullAsDefualt: true,
+    connection: {
+      filename: "./data/projects.db3",
+    },
     migrations: {
       directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/migrations",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
     },
   },
 };
