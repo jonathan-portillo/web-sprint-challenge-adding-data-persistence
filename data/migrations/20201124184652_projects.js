18d60eb569b3ed39666b38c6fc8809c9665703c4
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-11-24 19:19:09 -0500

    knex migrations

---
+++ data/migrations/20201124184652_projects.js
@@ -0,0 +1,41 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("id");
+      tbl.string("name", 128).notNullable();
+      tbl.string("description", 500);
+      tbl.boolean("completed").defaultTo(false).notNullable();
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("id");
+      tbl.string("description", 500).notNullable();
+      tbl.notes("notes", 500);
+      tbl.boolean("completed").defaultTo(false).notNullable();
+      tbl
+        .integer("project_id")
+        .references("id")
+        .inTable("projects")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("id");
+      tbl.string("name", 128).notNullable().unique();
+      tbl.string("description", 500);
+      tbl
+        .integer("project_id")
+        .references("id")
+        .inTable("projects")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("projects");
+};
